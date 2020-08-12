e2348332ed770ff05184366bb1cb1df3e630e643
Alexander Baranskiy <alexander.baranskiy@yandex.ru>
2020-08-12 16:55:10 +0300

    Splitted introduction commands

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ import note as Note
 import perms as Perms
 import mute as Mute
 import translate
+import introduction
 
 bot = telebot.TeleBot('1073948237:AAGKs3HzRBZwBZGkoQ5moJIakWQn39nQtX4')
 
@@ -37,38 +38,13 @@ forecasts = {}
 
 
 @bot.message_handler(commands=['start'])
-def command_handler(message):
-    bot.send_message(message.chat.id, 'Приветствую) Я бот-админ для чата.\nСправку по'
-                                      ' командам можно получить по команде /help@sanya_pilot_bot\nЕсли что-то не '
-                                      'работает, пните @alexander_baransky496\n')
+def start_wrapper(message):
+    introduction.start(message)
 
 
 @bot.message_handler(commands=['help'])
-def show_help(message):
-    bot.send_message(chat_id=message.chat.id,
-                     text='Список команд:\n'
-                          '/tr - Перевести сообщение. /tr <код языка, на который надо перевести>'
-                          '/notes - Показать список заметок\n'
-                          '/note - Просмотр заметки\n'
-                          '/addnote - Добавить заметку\n'
-                          '/delnote - Удалить заметку\n'
-                          '/mute - Мут навсегда (до размута)\n'
-                          '/tmute - Мут на время. Время прописывается в формате <кол-во><s/m/h/d>\n'
-                          '/unmute - Размут\n'
-                          '/ban - Забанить пользователя навсегда (до разбана)\n'
-                          '/banme - Забанить пользователя, написавшего команду\n'
-                          '/tban - Забанить пользователя на время. Формат такой же как в /tmute\n'
-                          '/unban - Разбан\n'
-                          '/kick - Кикнуть пользователя\n'
-                          '/kickme - Кикнуть пользователя, написавшего команду\n'
-                          '/restrict - Лишение пользователя всех прав\n'
-                          '/permit - Выдача пользователю всех прав\n'
-                          '/dpermit - Выдача пользователю дефолтных прав чата\n'
-                          '/demote - Лишение пользователя всех административных прав (пока не работает)\n'
-                          '/promote - Выдача пользователю всех административных прав (пока не работает)\n'
-                          '/weather - Показать текущую погоду. /weather <название города>'
-                          'Чтобы применить все эти команды, необходимо ответить командой на сообщение пользователя, '
-                          'которого вы хотите кикнуть, забанить и т. д.')
+def help_wrapper(message):
+    introduction.help(message)
 
 
 # Мут навсегда
