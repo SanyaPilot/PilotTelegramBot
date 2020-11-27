2125f3b64335fd633e6f8537824b5b3c7912f963
Alexander Baranskiy <alexander.baranskiy@yandex.ru>
2020-11-27 22:53:11 +0300

    Re-write ban to aiogram
    [NOT READY FOR USE]

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from aiogram import executor
 from init import dp
 
 #from modules import introduction, translate, ban, greeting, kick, messages, mute, note, perms, weather, admin
-from modules import introduction, mute, ban
+from modules import introduction, mute, ban, perms
 
 
 if __name__ == '__main__':
--- modules/perms.py
+++ modules/perms.py
@@ -1,142 +1,139 @@
-import telebot
-import config
-from translation import tw
-bot = telebot.TeleBot(config.token)
+from aiogram.types import Message
+from init import bot, dp, tw
 
 
-def restrict(message):
+@dp.message_handler(commands='restrict')
+async def restrict(message: Message):
     trans = tw.get_translation(message)
     if trans == 1:
         return
     try:
-        member = bot.get_chat_member(chat_id=message.chat.id,
-                                     user_id=message.from_user.id)
+        member = await bot.get_chat_member(chat_id=message.chat.id,
+                                           user_id=message.from_user.id)
         if member.status == 'creator' or member.status == 'administrator':
-            bot.restrict_chat_member(chat_id=message.chat.id,
-                                     user_id=message.reply_to_message.from_user.id,
-                                     until_date=0)
+            await bot.restrict_chat_member(chat_id=message.chat.id,
+                                           user_id=message.reply_to_message.from_user.id,
+                                           until_date=0)
 
-            bot.send_message(chat_id=message.chat.id,
-                             text=trans['perms']['restrict'].format(username=str(message.reply_to_message.from_user.username)))
+            await bot.send_message(chat_id=message.chat.id,
+                                   text=trans['perms']['restrict'].format(
+                                       username=str(message.reply_to_message.from_user.username)))
         else:
-            bot.reply_to(message, trans['global']['errors']['admin'])
+            await message.reply(trans['global']['errors']['admin'])
 
     except Exception:
-        bot.reply_to(message, trans['global']['errors']['default'])
+        await message.reply(trans['global']['errors']['default'])
 
 
-def permit(message):
+@dp.message_handler(commands='permit')
+async def permit(message: Message):
     trans = tw.get_translation(message)
     if trans == 1:
         return
     try:
-        member = bot.get_chat_member(chat_id=message.chat.id,
-                                     user_id=message.from_user.id)
+        member = await bot.get_chat_member(chat_id=message.chat.id,
+                                           user_id=message.from_user.id)
         if member.status == 'creator' or member.status == 'administrator':
-            bot.restrict_chat_member(chat_id=message.chat.id,
-                                     user_id=message.reply_to_message.from_user.id,
-                                     can_send_messages=True,
-                                     can_send_media_messages=True,
-                                     can_send_polls=True,
-                                     can_send_other_messages=True,
-                                     can_add_web_page_previews=True,
-                                     can_change_info=True,
-                                     can_invite_users=True,
-                                     can_pin_messages=True,
-                                     until_date=0)
+            await bot.restrict_chat_member(chat_id=message.chat.id,
+                                           user_id=message.reply_to_message.from_user.id,
+                                           can_send_messages=True,
+                                           can_send_media_messages=True,
+                                           can_send_other_messages=True,
+                                           can_add_web_page_previews=True,
+                                           until_date=0)
 
-            bot.send_message(chat_id=message.chat.id,
-                             text=trans['perms']['permit'].format(username=str(message.reply_to_message.from_user.username)))
+            await bot.send_message(chat_id=message.chat.id,
+                                   text=trans['perms']['permit'].format(
+                                       username=str(message.reply_to_message.from_user.username)))
         else:
-            bot.reply_to(message, trans['global']['errors']['admin'])
+            await message.reply(trans['global']['errors']['admin'])
 
     except Exception:
-        bot.reply_to(message, trans['global']['errors']['default'])
+        await message.reply(trans['global']['errors']['default'])
 
 
-def permit_default(message):
+@dp.message_handler(commands='dpermit')
+async def permit_default(message: Message):
     trans = tw.get_translation(message)
     if trans == 1:
         return
     try:
-        member = bot.get_chat_member(chat_id=message.chat.id,
-                                     user_id=message.from_user.id)
+        member = await bot.get_chat_member(chat_id=message.chat.id,
+                                           user_id=message.from_user.id)
         if member.status == 'creator' or member.status == 'administrator':
-            chat = bot.get_chat(chat_id=message.chat.id)
+            chat = await bot.get_chat(chat_id=message.chat.id)
             perms = chat.permissions
-            bot.restrict_chat_member(chat_id=message.chat.id,
-                                     user_id=message.reply_to_message.from_user.id,
-                                     can_send_messages=perms.can_send_messages,
-                                     can_send_media_messages=perms.can_send_media_messages,
-                                     can_send_polls=perms.can_send_polls,
-                                     can_send_other_messages=perms.can_send_other_messages,
-                                     can_add_web_page_previews=perms.can_add_web_page_previews,
-                                     can_change_info=perms.can_change_info,
-                                     can_invite_users=perms.can_invite_users,
-                                     can_pin_messages=perms.can_pin_messages,
-                                     until_date=0)
+            await bot.restrict_chat_member(chat_id=message.chat.id,
+                                           user_id=message.reply_to_message.from_user.id,
+                                           can_send_messages=perms.can_send_messages,
+                                           can_send_media_messages=perms.can_send_media_messages,
+                                           can_send_other_messages=perms.can_send_other_messages,
+                                           can_add_web_page_previews=perms.can_add_web_page_previews,
+                                           until_date=0)
 
-            bot.send_message(chat_id=message.chat.id,
-                             text=trans['perms']['permit_default'].format(username=str(
-                                                                        message.reply_to_message.from_user.username)))
+            await bot.send_message(chat_id=message.chat.id,
+                                   text=trans['perms']['permit_default'].format(username=str(
+                                       message.reply_to_message.from_user.username)))
         else:
-            bot.reply_to(message, trans['global']['errors']['admin'])
+            await message.reply(trans['global']['errors']['admin'])
 
     except Exception:
-        bot.reply_to(message, trans['global']['errors']['default'])
+        await message.reply(trans['global']['errors']['default'])
 
 
 # Убрать все права
-def demote(message):
+@dp.message_handler(commands='demote')
+async def demote(message: Message):
     trans = tw.get_translation(message)
     if trans == 1:
         return
     try:
-        member = bot.get_chat_member(chat_id=message.chat.id,
-                                     user_id=message.from_user.id)
+        member = await bot.get_chat_member(chat_id=message.chat.id,
+                                           user_id=message.from_user.id)
         if member.status == 'creator' or member.status == 'administrator':
-            bot.promote_chat_member(chat_id=message.chat.id,
-                                    user_id=message.reply_to_message.from_user.id,
-                                    can_pin_messages=False,
-                                    can_change_info=False,
-                                    can_invite_users=False,
-                                    can_delete_messages=False,
-                                    can_promote_members=False,
-                                    can_restrict_members=False
-                                    )
-            bot.send_message(chat_id=message.chat.id,
-                             text=trans['perms']['demote'].format(username=str(
-                                                                        message.reply_to_message.from_user.username)))
+            await bot.promote_chat_member(chat_id=message.chat.id,
+                                          user_id=message.reply_to_message.from_user.id,
+                                          can_pin_messages=False,
+                                          can_change_info=False,
+                                          can_invite_users=False,
+                                          can_delete_messages=False,
+                                          can_promote_members=False,
+                                          can_restrict_members=False
+                                          )
+            await bot.send_message(chat_id=message.chat.id,
+                                   text=trans['perms']['demote'].format(username=str(
+                                       message.reply_to_message.from_user.username)))
         else:
-            bot.reply_to(message, trans['global']['errors']['admin'])
+            await message.reply(trans['global']['errors']['admin'])
 
     except Exception:
-        bot.reply_to(message, trans['global']['errors']['default'])
+        await message.reply(trans['global']['errors']['default'])
 
 
 # Дать все права
-def promote(message):
+@dp.message_handler(commands='promote')
+async def promote(message: Message):
     trans = tw.get_translation(message)
     if trans == 1:
         return
     try:
-        member = bot.get_chat_member(chat_id=message.chat.id,
-                                     user_id=message.from_user.id)
+        member = await bot.get_chat_member(chat_id=message.chat.id,
+                                           user_id=message.from_user.id)
         if member.status == 'creator' or member.status == 'administrator':
-            bot.promote_chat_member(chat_id=message.chat.id,
-                                    user_id=message.reply_to_message.from_user.id,
-                                    can_pin_messages=True,
-                                    can_change_info=True,
-                                    can_invite_users=True,
-                                    can_delete_messages=True,
-                                    can_promote_members=True,
-                                    can_restrict_members=True
-                                    )
-            bot.send_message(chat_id=message.chat.id,
-                             text=trans['perms']['promote'].format(username=str(
-                                                                        message.reply_to_message.from_user.username)))
+            await bot.promote_chat_member(chat_id=message.chat.id,
+                                          user_id=message.reply_to_message.from_user.id,
+                                          can_pin_messages=True,
+                                          can_change_info=True,
+                                          can_invite_users=True,
+                                          can_delete_messages=True,
+                                          can_promote_members=True,
+                                          can_restrict_members=True
+                                          )
+            await bot.send_message(chat_id=message.chat.id,
+                                   text=trans['perms']['promote'].format(username=str(
+                                       message.reply_to_message.from_user.username)))
         else:
-            bot.reply_to(message, trans['global']['errors']['admin'])
+            await message.reply(trans['global']['errors']['admin'])
 
     except Exception:
-        bot.reply_to(message, trans['global']['errors']['default'])
+        await message.reply(trans['global']['errors']['default'])
