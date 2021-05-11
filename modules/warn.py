213d4eb9ed545ce1044c929a51af77c1affd25d9
AlexanderBaransky <alexander.baranskiy@yandex.ru>
2021-05-11 18:11:20 +0300

    warns: Add /resetwarns command

--- modules/warn.py
+++ modules/warn.py
@@ -67,7 +67,7 @@ async def warn(message: Message, user_obj=None):
             return
 
         if check_perms and message.from_user.id == user.id:
-            await message.reply(trans['warn']['same_usr_err'])
+            await message.reply(trans['warn']['same_usr_err'][0])
             logger.warning(f"{message.chat.full_name}: User {username} wanted to warn himself")
             return
 
@@ -198,3 +198,87 @@ async def apply_punishment(message: Message, user, trans, name, member):
         await bot.send_message(chat_id=message.chat.id,
                                text=trans['kick']['kick'].format(
                                     username=str(name)))
+
+
+@dp.message_handler(commands='resetwarns')
+async def reset_warns(message: Message):
+    trans = tw.get_translation(message)
+    if trans == 1:
+        return
+    try:
+        punishment = session.query(Chats.warns_punishment).filter_by(chat_id=message.chat.id).first()
+        if punishment[0] is None:
+            return
+        check_perms = True
+        user = None
+        username = None
+        text_mention = False
+        for entity in message.entities:
+            if entity.type == 'mention':
+                if not '--force' in message.text:
+                    user = message.text[entity.offset:]
+                else:
+                    user = message.text[entity.offset:-7]
+
+            elif entity.type == 'text_mention':
+                user = entity.user
+                text_mention = True
+
+        if user and not text_mention:
+            try:
+                user = await get_user(user)
+            except ValueError:
+                await message.reply(text=trans['global']['errors']['user_not_found'])
+                return
+
+            if not user.last_name:
+                username = user.first_name
+            else:
+                username = user.first_name + user.last_name
+
+        elif not user and message.reply_to_message:
+            user = message.reply_to_message.from_user
+            username = user.full_name
+        elif text_mention:
+            username = user.full_name
+        else:
+            await message.reply(trans['global']['errors']['no_args/reply'])
+            logger.warning(
+                f'{message.chat.full_name}: User {message.from_user.full_name} tried to use command without args/reply')
+            return
+
+        member = await bot.get_chat_member(chat_id=message.chat.id,
+                                           user_id=message.from_user.id)
+        if check_perms and not (member.status == 'creator' or member.status == 'administrator'):
+            await message.reply(trans['global']['errors']['admin'])
+            logger.warning(
+                f"{message.chat.full_name}: User {message.from_user.full_name} need administrative privileges to do this")
+            return
+
+        if check_perms and message.from_user.id == user.id:
+            await message.reply(trans['warn']['same_usr_err'][1])
+            logger.warning(f"{message.chat.full_name}: User {username} wanted to warn himself")
+            return
+
+        me = await bot.get_me()
+        if not user.id == me.id:
+            data = session.query(Warns).filter_by(chat_id=message.chat.id, user_id=user.id).first()
+            name = None
+            if user.username:
+                name = '@' + user.username
+            else:
+                name = username
+            if data:
+                session.query(Warns).filter_by(user_id=user.id, chat_id=message.chat.id).delete()
+                session.commit()
+                await message.reply(text=trans['warn']['reset'].format(name), parse_mode='HTML')
+            else:
+                await message.reply(trans['warn']['no_warns_err'])
+
+        else:
+            await message.reply(trans['global']['errors']['affect_on_bot'])
+            logger.warning(f"{message.chat.full_name}: User {message.from_user.full_name} wanted to warn bot")
+
+    except Exception as err:
+        await message.reply(trans['global']['errors']['default'])
+        logger.error(f"{message.chat.full_name}: User {message.from_user.full_name} {err}")
